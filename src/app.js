afbb6a8f3dd744d51c67e205b718501e104d9a40
innoma-anayo <innocenteziefula2@gmail.com>
2026-09-06 17:02:45 +0100

    add API health check

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-  require("dotenv").config();
+require("dotenv").config();
+
 const express = require("express");
 
 const connectDB = require("./config/db");
@@ -6,35 +7,34 @@ const logger = require("./middlewares/logger");
 const errorHandler = require("./middlewares/errorHandler");
 const noteRoutes = require("./routes/noteRoutes");
 
-
 const app = express();
-connectDB();
 
+// Middleware
 app.use(express.json());
-app.use("/api/notes",noteRoutes);
+
+// Health check
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Personal Knowledge Base API is running"
+    });
+});
+
+// API routes
+app.use("/api/notes", noteRoutes);
+
+// Logger
 app.use(logger);
+
+// Error handler
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+// Start server
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Connect to MongoDB
+connectDB();
